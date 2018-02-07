3923fc953d106afd8eb7160cda7d586ffb583eca
Armando Perez <gmandx@gmail.com>
2018-02-06 23:32:11 -0800

    fish/fzf tweaks

--- .config/fish/conf.d/mandx.fish
+++ .config/fish/conf.d/mandx.fish
@@ -7,18 +7,23 @@ alias grep='grep --color=auto '
 alias ll='ls --color -alF '
 alias psgrep='ps -ef | grep -i '
 
-# Misc
+# Globals
 set -x VISUAL 'subl --wait '
 set -x LESS '--ignore-case --raw-control-chars '
 set -x BROWSER 'firefox '
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 
-set -g default_user mandx
-set -g theme_display_git yes
-set -g theme_display_hg no
-set -g theme_display_ruby no
-set -g theme_display_user no
-set -g theme_display_virtualenv yes
-set -g theme_avoid_ambiguous_glyphs yes
-set -g theme_display_cmd_duration yes
-set -g theme_display_k8s_context no
+# Theme (bobthefish) options
+set -U default_user mandx
+set -U theme_display_git yes
+set -U theme_display_hg no
+set -U theme_display_ruby no
+set -U theme_display_user no
+set -U theme_display_virtualenv yes
+set -U theme_avoid_ambiguous_glyphs yes
+set -U theme_display_cmd_duration yes
+set -U theme_display_k8s_context no
+
+# fzf options
+set -U FZF_LEGACY_KEYBINDINGS 0
+
