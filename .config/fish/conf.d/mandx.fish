5ee8c6cdab5f607788b2a123a000a8564bb1e3c6
Armando Perez <gmandx@gmail.com>
2019-06-14 22:04:39 -0700

    Add .gitignore_global

--- .config/fish/conf.d/mandx.fish
+++ .config/fish/conf.d/mandx.fish
@@ -1,9 +1,15 @@
 # Prevent fish from altering the X clipboard
 set FISH_CLIPBOARD_CMD "cat"
 
+# Aliases
+alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME '
+alias grep='grep --color=auto '
+alias psgrep='ps -ef | grep -i '
+
 # Cache the platform name
 set -l __uname (uname)
 
+# Determine which style of color flag to use for `ls`
 switch $__uname
 case Linux
     set -g __ls__color_flag '--color'
@@ -13,22 +19,11 @@ case '*'
     set -g __ls__color_flag ''
 end
 
-# Aliases
-alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME '
-alias grep='grep --color=auto '
-alias psgrep='ps -ef | grep -i '
-
 # Can't make this variable expansion to work in an alias definition...
 # alias ll='ls $__ls__color_flag -alF '
 # So let's use a function for now
 function ll
     ls $__ls__color_flag -alF $argv
-end
-
-# This is just a workaround for a visual glitch in Linux
-if [ $__uname != "Linux" ] 
-    alias gitg='env GTK_THEME=Arc-Darker gitg'
-    alias meld='env GTK_THEME=Arc-Darker meld'
 end
 
 # Globals
@@ -49,6 +44,6 @@ set -U theme_display_cmd_duration yes
 set -U theme_display_k8s_context no
 set -U theme_display_git_master_branch yes
 
-# fzf options
-set -U FZF_LEGACY_KEYBINDINGS 1
+# Needed for pipx
+set -gx PATH $PATH $HOME/.local/bin
 
